ea0a62d7d2b4df9bf3847a6a259a496df6e53cf9
Fernando Medvedovsky <fernandodavidmedve56@gmail.com>
2024-05-24 00:04:52 -0300

    Punto 6 terminado

--- tp3_ia/punto6.py
+++ tp3_ia/punto6.py
@@ -1,16 +1,7 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score, cross_validate
 from sklearn.linear_model import LogisticRegression
-
-columns = ["ID","Sex","Edad","SalarioEstimado","Compra"]
-
-dataset = pd.read_csv("ClientesEnLinea.csv", header=0, names=columns)
-
-feature_cols = ["ID","Sex","Edad","SalarioEstimado"]
-
-x = dataset[feature_cols]
-y = dataset["Compra"]
-
-model = LogisticRegression(random_state=1, max_iter=10000)
+from sklearn.preprocessing import LabelEncoder
 
 # Dataset ClientesEnLinea.csv
 # Crear un modelo de Regresión Logística utilizando el dataset ClientesEnLinea.csv que cuenta con in-
@@ -19,6 +10,80 @@ model = LogisticRegression(random_state=1, max_iter=10000)
 # compró respectivamente.
 # Utilizar como métrica comparativa el promedio de una validación cruzada K-fold con 5 folds para
 # entrenamiento y testeo.
+
+df = pd.read_csv("ClientesEnLinea.csv")
+
+print(df.head())
+print("\n")
+
+le = LabelEncoder()
+df["Sex"] = le.fit_transform(df["Sex"])
+
+print(df.head())
+print("\n")
+
+x = df[["Sex", "Edad", "SalarioEstimado"]]
+y = df["Compra"]
+
+
+model = LogisticRegression(max_iter=10000)
+
+
 # (a) ¿Cómo se comporta el modelo si consideramos todos los predictores?
+print(10 * "-", "PUNTO A", 10 * "-")
+scores = cross_validate(
+    model, x, y, scoring=["accuracy", "precision", "f1", "recall"], cv=5
+)
+
+print("Accuracies:", scores["test_accuracy"])
+print("Precisions:", scores["test_precision"])
+print("Recalls:", scores["test_recall"])
+print("F1s:", scores["test_f1"])
+
+print("Mean accuracy:", scores["test_accuracy"].mean())
+print("Mean Precision:", scores["test_precision"].mean())
+print("Mean Recall:", scores["test_recall"].mean())
+print("Mean F1:", scores["test_f1"].mean())
+
+"""
+Si consideramos todos los predictores, tras la validación cruzada de 5
+K-Folds el modelo muestra:
+- Una exactitud promedio de ~82%
+- Una precisión promedio de ~80%
+- Un recall promedio de ~68%
+- Un F1 promedio de ~70%
+Por lo que el modelo podría considerarse que no es tan bueno a la hora de
+clasificar a los clientes.
+"""
+
 # (b) ¿Qué sucede cuando solo consideramos como predictores Sexo y Edad?
 
+print("\n")
+print(10 * "-", "PUNTO B", 10 * "-")
+
+mod_x = x.drop(["SalarioEstimado"], axis=1)
+
+print("mod_x: \n", mod_x.head())
+print("\n")
+
+scores = cross_validate(
+    model, mod_x, y, scoring=["accuracy", "precision", "f1", "recall"], cv=5
+)
+
+print("Accuracies:", scores["test_accuracy"])
+print("Precisions:", scores["test_precision"])
+print("Recalls:", scores["test_recall"])
+print("F1s:", scores["test_f1"])
+
+print("Mean accuracy:", scores["test_accuracy"].mean())
+print("Mean Precision:", scores["test_precision"].mean())
+print("Mean Recall:", scores["test_recall"].mean())
+print("Mean F1:", scores["test_f1"].mean())
+
+
+"""
+Cuando sólo consideramos como predictores el Sexo y la Edad del cliente
+obtenemos resultados muy similares a los obtenidos utilizando todos los
+predictores. A partir de esto podemos observar que el predictor "SalarioEstimado"
+no aporta mucho a la hora de realizar las predicciones.
+"""
